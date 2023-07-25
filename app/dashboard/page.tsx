58debc373ea3bd9ac5d9e4eef7ed132b012d8a80
FF <focused.fault@gmail.com>
2023-07-25 05:55:57 -0400

    upload

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </main>
+  );
+}
